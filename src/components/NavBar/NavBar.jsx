67d4d7c7c2eebe481042743c6473633a70943550
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2023-11-10 20:27:59 +0100

    added navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+// NavBar.js
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="logo">Logo</div>
+      <div className="spacer"></div>
+      <div className="nav-links-wrapper">
+        <a href="#" className="nav-link">Log-in</a>
+        <a href="#" className="nav-link">Blog</a>
+        <a href="#" className="nav-link">Chat</a>
+        <a href="#" className="nav-link">Kontakt</a>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
