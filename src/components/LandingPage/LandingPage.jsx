6f8d04017d1a36e1f5b42a247c36a1ae196e4266
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2023-11-21 22:42:01 +0100

    just some text

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,9 +4,65 @@ import "./LandingPage.css";
 function LandingPage() {
   return (
     <>
-    
       <section className="wrapper">
-       
+        <div className="top-box">
+          <h1>Text</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            distinctio esse, aliquam, culpa sint suscipit ducimus tempore ea
+            mollitia, sapiente ullam eaque consequatur neque exercitationem?
+            Numquam, sequi placeat quasi fugit, vitae porro animi mollitia,
+            praesentium quisquam illum dolores ducimus earum eaque nostrum.
+            Tempore quas aperiam nulla magni soluta dolores et, sunt nobis hic
+            neque consectetur sed ullam maxime doloribus praesentium incidunt
+            quod impedit voluptatum fugit! Nulla molestias debitis consequuntur
+            fuga deleniti dolore quo recusandae illo inventore alias temporibus
+            corporis praesentium adipisci, deserunt consequatur, est aliquid
+            tempora similique voluptatem iste? Officia, tempore ullam. Debitis
+            rem soluta iste eius impedit qui quam?
+          </p>
+          <div className="top-img">
+            <img src="https://placehold.co/600x400" alt="" />
+          </div>
+        </div>
+        <div className="mid-box">
+          <h2>Text</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            facilis excepturi in, temporibus earum, voluptas eaque natus dicta
+            libero blanditiis dolore id quos maiores assumenda aliquam hic porro
+            animi quaerat itaque autem! Eius sit ratione ipsum magnam magni
+            tempora aut commodi. Minima officiis ab quas totam saepe laudantium
+            quisquam et, excepturi tempora nemo incidunt delectus. Quia iusto
+            voluptatum harum at? Aliquam beatae sit tempore illum voluptatibus
+            est doloremque explicabo doloribus et, labore sed esse eveniet,
+            officiis nihil a dolore? Repudiandae, saepe magnam! Enim, omnis
+            ipsum! Pariatur accusantium voluptatum asperiores, omnis maxime
+            rerum vel provident saepe cumque ipsum expedita error dolor!
+          </p>
+        </div>
+        <div className="mid-img">
+          <img src="https://placehold.co/600x400" alt="" />
+        </div>
+        <div className="bottom-box">
+          <h3>Text</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            facilis excepturi in, temporibus earum, voluptas eaque natus dicta
+            libero blanditiis dolore id quos maiores assumenda aliquam hic porro
+            animi quaerat itaque autem! Eius sit ratione ipsum magnam magni
+            tempora aut commodi. Minima officiis ab quas totam saepe laudantium
+            quisquam et, excepturi tempora nemo incidunt delectus. Quia iusto
+            voluptatum harum at? Aliquam beatae sit tempore illum voluptatibus
+            est doloremque explicabo doloribus et, labore sed esse eveniet,
+            officiis nihil a dolore? Repudiandae, saepe magnam! Enim, omnis
+            ipsum! Pariatur accusantium voluptatum asperiores, omnis maxime
+            rerum vel provident saepe cumque ipsum expedita error dolor!
+          </p>
+        </div>
+        <div className="bottom-img">
+          <img src="https://placehold.co/600x400" alt="" />
+        </div>
       </section>
     </>
   );
